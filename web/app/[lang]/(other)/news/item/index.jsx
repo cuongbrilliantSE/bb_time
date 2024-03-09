67866b9d62cc674f254567cb50c2740b095747bc
cuongdoan <doanmanhcuong1234567@gmail.com>
2024-03-09 15:55:40 +0700

    add animation

--- web/app/[lang]/(other)/news/item/index.jsx
+++ web/app/[lang]/(other)/news/item/index.jsx
@@ -25,18 +25,18 @@ const PostItem = ({ data, i18n }) => {
 
   return (
     <div className={styles.wp}>
-      <div className={clsx(styles.img, "animation")}>
+      <div className={clsx(styles.img, "animation", "fade-in-down")}>
         <ImageResponsive data={data.thumb.data.attributes} />
       </div>
       <span
         onClick={() => goToNewsDetail(data.slug)}
-        className={clsx(styles.title, "animation")}
+        className={clsx(styles.title, "animation", "fade-in-down")}
         style={{cursor: 'pointer'}}
       >
         {data.title}
       </span>
       <div className={styles.des}>
-        <div className={clsx(styles.author, "animation")}>
+        <div className={clsx(styles.author, "animation", "fade-in-down")}>
           <div className={styles.authorAvatar}>
             <img
               src={getImgUrl(
@@ -48,21 +48,21 @@ const PostItem = ({ data, i18n }) => {
             {data?.author?.data?.attributes?.username}
           </p>
         </div>
-        <div className={clsx(styles.divider, "animation")}></div>
-        <div className={clsx(styles.updatedAt, "animation")}>
+        <div className={clsx(styles.divider, "animation", "fade-in-down")}></div>
+        <div className={clsx(styles.updatedAt, "animation", "fade-in-down")}>
           <img src={CalendarImg.src} />
           <p>{format(new Date(data?.updatedAt), "dd LLLL yyyy")}</p>
         </div>
-        <div className={clsx(styles.divider, "animation")}></div>
-        <div className={clsx(styles.minRead, "animation")}>
+        <div className={clsx(styles.divider, "animation", "fade-in-down")}></div>
+        <div className={clsx(styles.minRead, "animation", "fade-in-down")}>
           <img src={ClockImg.src} />
           <p>3 min. to read</p>
         </div>
       </div>
-      <p className={clsx(styles.desTxt, "animation")}>{data.description}</p>
+      <p className={clsx(styles.desTxt, "animation", "fade-in-down")}>{data.description}</p>
       <Link
         href={`/news/${data.slug}`}
-        className={clsx(styles.detailBtn, "animation")}
+        className={clsx(styles.detailBtn, "animation", "fade-in-down")}
       >
         <p>{i18n.view_detail}</p>
         <ChevronRightImg />
